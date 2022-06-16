30c477ce9f260df18e514c9892f6cdfcd7e31f24
Andrey Aristov <aaristov@pasteur.fr>
2022-06-16 16:26:45 +0200

    fix test

--- src/adc/count.py
+++ src/adc/count.py
@@ -1,160 +1,187 @@
+import logging
 from functools import partial
+
+import fire
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage.feature import peak_local_max
-from skimage.feature import peak
-from scipy import ndimage as ndi
-from tifffile import imread, imwrite
-import fire
 import pandas as pd
+from scipy import ndimage as ndi
+from skimage.feature import peak_local_max
 from skimage.measure import regionprops
-import logging
+from tifffile import imread
 
 logger = logging.getLogger(__name__)
 
 
 def get_cell_numbers(
-    multiwell_image:np.ndarray, 
-    labels:np.ndarray, 
-    plot=False, 
-    threshold_abs:float=2, 
-    min_distance:float=5,
+    multiwell_image: np.ndarray,
+    labels: np.ndarray,
+    plot=False,
+    threshold_abs: float = 2,
+    min_distance: float = 5,
     dif_gauss_sigma=(3, 5),
-    meta:dict=None,
-    bf:np.ndarray=None
-    ) -> pd.DataFrame:
-    '''
+    bf: np.ndarray = None,
+    **kwargs,
+) -> pd.DataFrame:
+    """
     Counting fluorescence peaks inside the labels.
     The data is filtered by gaussian difference filter first.
     The table with labels, coordinates and number of particles returned.
-    '''
-    
+    """
+
     props = regionprops(labels)
 
     def get_n_peaks(i):
+
         if bf is None:
             return get_peak_number(
-                multiwell_image[props[i].slice], 
-                plot=plot, 
-                dif_gauss_sigma=dif_gauss_sigma, 
-                threshold_abs=threshold_abs, 
+                multiwell_image[props[i].slice],
+                plot=plot,
+                dif_gauss_sigma=dif_gauss_sigma,
+                threshold_abs=threshold_abs,
                 min_distance=min_distance,
-                title=props[i].label
-                )
+                title=props[i].label,
+            )
         else:
             return get_peak_number(
-                multiwell_image[props[i].slice], 
-                plot=plot, 
-                dif_gauss_sigma=(3, 5), 
-                threshold_abs=threshold_abs, 
+                multiwell_image[props[i].slice],
+                plot=plot,
+                dif_gauss_sigma=dif_gauss_sigma,
+                threshold_abs=threshold_abs,
                 min_distance=min_distance,
                 title=props[i].label,
                 bf_crop=bf[props[i].slice],
-                return_std=True
-                )
+                return_std=True,
+            )
 
     n_cells = list(map(get_n_peaks, range(labels.max())))
-    return pd.DataFrame([{
-        'label': prop.label, 
-        'x': prop.centroid[1], 
-        'y': prop.centroid[0], 
-        'n_cells': n_cell[0],
-        # 'std': n_cell[1],
-        **meta
-        } for prop, n_cell in zip(props, n_cells)])
+    # return n_cells, props
+    return pd.DataFrame(
+        [
+            {
+                "label": prop.label,
+                "x": prop.centroid[1],
+                "y": prop.centroid[0],
+                "n_cells": n_cell[0],
+                **kwargs,
+            }
+            for prop, n_cell in zip(props, n_cells)
+        ]
+    )
 
 
-
-def crop(stack:np.ndarray, center:tuple, size:int):
-    im = stack[:, int(center[0]) - size//2:int(center[0]) + size//2, int(center[1]) - size//2:int(center[1]) + size//2]
+def crop(stack: np.ndarray, center: tuple, size: int):
+    im = stack[
+        :,
+        int(center[0]) - size // 2 : int(center[0]) + size // 2,
+        int(center[1]) - size // 2 : int(center[1]) + size // 2,
+    ]
     return im
 
 
 def gdif(array2d, dif_gauss_sigma=(1, 3)):
-    array2d = array2d.astype('f')
-    return ndi.gaussian_filter(array2d, sigma=dif_gauss_sigma[0]) - ndi.gaussian_filter(array2d, sigma=dif_gauss_sigma[1])
+    array2d = array2d.astype("f")
+    return ndi.gaussian_filter(
+        array2d, sigma=dif_gauss_sigma[0]
+    ) - ndi.gaussian_filter(array2d, sigma=dif_gauss_sigma[1])
 
 
-def get_peak_number(crop2d, dif_gauss_sigma=(1, 3), min_distance=3, threshold_abs=5, plot=False, 
-                    title='', bf_crop=None, return_std=False):
+def get_peak_number(
+    crop2d,
+    dif_gauss_sigma=(1, 3),
+    min_distance=3,
+    threshold_abs=5,
+    plot=False,
+    title="",
+    bf_crop=None,
+    return_std=False,
+):
     image_max = gdif(crop2d, dif_gauss_sigma)
-    peaks = peak_local_max(image_max, min_distance=min_distance, threshold_abs = threshold_abs)
-    
+    peaks = peak_local_max(
+        image_max, min_distance=min_distance, threshold_abs=threshold_abs
+    )
+    logger.debug(f"found {len(peaks)} cells")
     if plot:
         if bf_crop is None:
             fig, ax = plt.subplots(1, 2, sharey=True)
             ax[0].imshow(crop2d)
-            ax[0].set_title(f'raw image {title}')
+            ax[0].set_title(f"raw image {title}")
             ax[1].imshow(image_max)
-            ax[1].set_title('Filtered + peak detection')
-            ax[1].plot(peaks[:,1], peaks[:,0], 'r.')
+            ax[1].set_title("Filtered + peak detection")
+            ax[1].plot(peaks[:, 1], peaks[:, 0], "r.")
             plt.show()
         else:
             fig, ax = plt.subplots(1, 3, sharey=True)
 
-            ax[0].imshow(bf_crop, cmap='gray')
-            ax[0].set_title(f'BF {title}')
-            
+            ax[0].imshow(bf_crop, cmap="gray")
+            ax[0].set_title(f"BF {title}")
+
             ax[1].imshow(crop2d, vmax=crop2d.mean() + 2 * crop2d.std())
-            ax[1].set_title(f'raw image {title}')
+            ax[1].set_title(f"raw image {title}")
 
             ax[2].imshow(image_max)
-            ax[2].set_title(f'Filtered + {len(peaks)} peaks (std {image_max.std():.2f})')
-            ax[2].plot(peaks[:,1], peaks[:,0], 'r.')
+            ax[2].set_title(
+                f"Filtered + {len(peaks)} peaks (std {image_max.std():.2f})"
+            )
+            ax[2].plot(peaks[:, 1], peaks[:, 0], "r.")
             plt.show()
 
     if return_std:
         return len(peaks), crop2d.std()
     else:
-        return (len(peaks), )
+        return (len(peaks),)
 
 
 def get_peaks_per_frame(stack3d, dif_gauss_sigma=(1, 3), **kwargs):
     image_ref = gdif(stack3d[0], dif_gauss_sigma)
     thr = 5 * image_ref.std()
-    return list(map(partial(get_peak_number, threshold_abs=thr, **kwargs), stack3d))
+    return list(
+        map(partial(get_peak_number, threshold_abs=thr, **kwargs), stack3d)
+    )
 
 
 def get_peaks_all_wells(stack, centers, size, plot=0):
     n_peaks = []
     for c in centers:
-        print('.', end='')
-        well = crop(stack, c['center'], size)
+        print(".", end="")
+        well = crop(stack, c["center"], size)
         n_peaks.append(get_peaks_per_frame(well, plot=plot))
     return n_peaks
 
-    
 
 def main(
-    aligned_path:str, 
-    gaussian_difference_filter:tuple=(3,5), 
-    threshold:float=2, 
-    min_distance:float=5, 
-    save_path_csv:str="", 
+    aligned_path: str,
+    gaussian_difference_filter: tuple = (3, 5),
+    threshold: float = 2,
+    min_distance: float = 5,
+    save_path_csv: str = "",
     plot=False,
-    **kwargs
+    **kwargs,
 ):
-    
-    '''
+
+    """
     Reads the data and saves the counting table
-    '''
+    """
     bf, fluo, mask = imread(aligned_path)
     table = get_cell_numbers(
-        multiwell_image=fluo, 
-        labels=mask, 
-        threshold_abs=threshold, 
-        min_distance=min_distance, 
+        multiwell_image=fluo,
+        labels=mask,
+        threshold_abs=threshold,
+        min_distance=min_distance,
         dif_gauss_sigma=gaussian_difference_filter,
-        bf=bf, 
+        bf=bf,
         plot=plot,
-        meta=kwargs
+        meta=kwargs,
     )
-    if save_path_csv.endswith('.csv'):
+    if save_path_csv.endswith(".csv"):
         table.to_csv(save_path_csv, index=None)
-        logger.info(f'Saved table to {save_path_csv}')
+        logger.info(f"Saved table to {save_path_csv}")
     else:
-        logger.warning('No path to save the table! Provide `save_path_csv` parameter')
+        logger.warning(
+            "No path to save the table! Provide `save_path_csv` parameter"
+        )
     return table
+
 
 if __name__ == "__main__":
     fire.Fire(main)
