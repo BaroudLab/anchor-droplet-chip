f130f5f7f334d2dfd8a9736b71d11fd4dde0334a
Andrey Aristov <aaristov@pasteur.fr>
2022-07-28 16:11:05 +0200

    merge by pairs

--- src/adc/merge.py
+++ src/adc/merge.py
@@ -5,6 +5,10 @@ from importlib.metadata import PackageNotFoundError, version
 import fire
 import pandas as pd
 
+from adc import plot
+
+logging.basicConfig(level="INFO")
+
 logger = logging.getLogger("adc.merge")
 try:
     __version__ = version("anchor-droplet-chip")
@@ -14,8 +18,13 @@ except PackageNotFoundError:
 
 
 def merge(
-    counts_day1: pd.DataFrame, counts_day2: pd.DataFrame
+    counts_day1: pd.DataFrame,
+    counts_day2: pd.DataFrame,
+    concentration: float = None,
 ) -> pd.DataFrame:
+    import sys
+
+    print("args: ", sys.argv)
     """
     Copies the first input into the output and adds the
     `n_cells` from the second input into the output as `n_cells_final`
@@ -24,11 +33,18 @@ def merge(
     logger.info(f"Copy day1 to the output")
     table.loc[:, "n_cells_final"] = counts_day2.n_cells
     logger.info("Added the the column `n_cells_final` from day2 to the output")
+    if concentration is not None:
+        table.loc[:, "concentration"] = concentration
+        logger.info(f"Add concentration: {concentration}")
+    else:
+        logger.info("No concentration")
     return table
 
 
 def merge_csv(
-    csv_day1: str, csv_day2: str, csv_out: str = "", concentration=None
+    csv_day1: str,
+    csv_day2: str,
+    csv_out: str = "",
 ):
     """
     Reads the data and performs merging (see ads.merge.merge)
@@ -37,7 +53,6 @@ def merge_csv(
         csv_out = os.path.join(
             os.path.commonpath([csv_day1, csv_day2]), "table.csv"
         )
-    logging.basicConfig(level="INFO")
     fh = logging.FileHandler(log_path := csv_out.replace(".csv", ".log"))
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s : %(message)s"
@@ -54,14 +69,69 @@ def merge_csv(
         counts_day2 = pd.read_csv(csv_day2)
         logger.info(f"Merging both")
         table = merge(counts_day1=counts_day1, counts_day2=counts_day2)
-        if concentration is not None:
-            table.loc["concentration"] = concentration
         logger.info(f"Saving the output: {csv_out}")
         table.to_csv(csv_out)
+
     except Exception as e:
         logger.error(f"Merge failed due to {e}")
         raise e
 
 
+def merge_all(
+    paths_day1: "list[str]",
+    paths_day2: "list[str]",
+    concentrations: "list[int]",
+    threshold: int = 10,
+    table_path: str = "",
+    swarm_path: str = "",
+    prob_path: str = "",
+    prob_log_path: str = "",
+) -> None:
+    fh = logging.FileHandler(
+        log_path := str(table_path).replace(".csv", ".log")
+    )
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s : %(message)s"
+    )
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    logger.debug(
+        "args1: ",
+        paths_day1,
+    )
+    logger.debug(
+        "args2: ",
+        paths_day2,
+    )
+    logger.debug("c:", concentrations)
+    logger.debug("out:", table_path)
+    m = [
+        merge(
+            counts_day1=pd.read_csv(d1),
+            counts_day2=pd.read_csv(d2),
+            concentration=c,
+        )
+        for d1, d2, c in zip(paths_day1, paths_day2, concentrations)
+    ]
+    df = pd.concat(m, ignore_index=True)
+
+    df.loc[:, "final_state"] = df.n_cells_final > threshold
+    df.loc[:, "threshold"] = threshold
+
+    logger.debug(df.head())
+    if table_path:
+        df.to_csv(str(table_path))
+        logger.info(f"Saved table: {table_path}")
+
+    n_max = int(df.n_cells.mean() * 3)
+
+    plot._swarm_counts(
+        df, n_max=n_max, threshold=threshold, path=str(swarm_path)
+    )
+    plot._probs(df, n_max=n_max, path=str(prob_path))
+    plot._probs_log(df, n_max=n_max, path=str(prob_log_path))
+
+
 if __name__ == "__main__":
-    fire.Fire(merge_csv)
+    fire.Fire(merge_all)
