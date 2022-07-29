e9aa66603ae1fa9098bd6efe716ce8d943194714
Andrey Aristov <aaristov@pasteur.fr>
2022-07-29 12:26:54 +0200

    remove import

--- scripts/merge_all.py
+++ scripts/merge_all.py
@@ -1,14 +1,14 @@
-import snakemake
+# import snakemake
 
 from adc.merge import merge_all
 
 merge_all(
-    paths_day1=snakemake.input.day1,
-    paths_day2=snakemake.input.day2,
-    concentrations=snakemake.params.concentrations,
-    threshold=snakemake.config["threshold"],
-    table_path=snakemake.output.table,
-    swarm_path=snakemake.output.swarm,
-    prob_path=snakemake.output.prob,
-    prob_log_path=snakemake.output.prob_log,
+    paths_day1=snakemake.input.day1,  # noqa: F821
+    paths_day2=snakemake.input.day2,  # noqa: F821
+    concentrations=snakemake.params.concentrations,  # noqa: F821
+    threshold=snakemake.config["threshold"],  # noqa: F821
+    table_path=snakemake.output.table,  # noqa: F821
+    swarm_path=snakemake.output.swarm,  # noqa: F821
+    prob_path=snakemake.output.prob,  # noqa: F821
+    prob_log_path=snakemake.output.prob_log,  # noqa: F821
 )
