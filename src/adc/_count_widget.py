1377a5a2bf167548a1270c43b452870cec1054d7
Andrey Aristov <aaristov@pasteur.fr>
2022-10-05 11:04:17 +0200

    error handling for centers

--- src/adc/_count_widget.py
+++ src/adc/_count_widget.py
@@ -1,27 +1,13 @@
-from operator import add
-import os
-from threading import Thread
-
-import dask.array as da
-from magicgui.widgets import create_widget, Slider
+import numpy as np
+import pandas as pd
+from magicgui.widgets import create_widget
 from napari import Viewer
 from napari.layers import Image, Points
-from napari.utils.notifications import show_info
 from napari.utils import progress
-from napari.qt import thread_worker
-from qtpy.QtWidgets import (
-    QCheckBox,
-    QLabel,
-    QLineEdit,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-    
-)
+from napari.utils.notifications import show_error, show_info
+from qtpy.QtWidgets import QLineEdit, QPushButton, QVBoxLayout, QWidget
 
 from adc import count
-import pandas as pd
-import numpy as np
 
 
 class CountCells(QWidget):
@@ -36,7 +22,6 @@ class CountCells(QWidget):
         )
         self.radius = 300
         self.select_centers = create_widget(label="centers", annotation=Points)
-
 
         self.out_path = ""
         self.output_filename_widget = QLineEdit("path")
@@ -54,30 +39,46 @@ class CountCells(QWidget):
 
         self.setLayout(self.layout)
 
-    
     def _update_detections(self):
-        show_info('Loading the data')
+        show_info("Loading the data")
         with progress(desc="Loading data") as prb:
-            fluo = self.viewer.layers[self.select_TRITC.current_choice].data[0].compute() # max resolution
+            fluo = (
+                self.viewer.layers[self.select_TRITC.current_choice]
+                .data[0]
+                .compute()
+            )  # max resolution
         centers = self.viewer.layers[self.select_centers.current_choice].data
-        self.df = pd.DataFrame(data=centers, columns=["chip", "y", "x"])
-        show_info('Data loaded. Counting')
+        try:
+            self.df = pd.DataFrame(data=centers, columns=["chip", "y", "x"])
+        except ValueError:
+            show_error("Choose the right layer with actual localizations")
+            return
+        show_info("Data loaded. Counting")
         counts = []
         detections = []
         self.viewer.window._status_bar._toggle_activity_dock(True)
-        for i, r in progress(self.df.iterrows(), total=2500, desc="wells"):
-            out = count.get_peak_number(count.crop2d(fluo[int(r.chip)], (r.y,r.x), self.radius), return_pos=True)
+        for i, r in progress(self.df.iterrows(), total=3000, desc="wells"):
+            out = count.get_peak_number(
+                count.crop2d(fluo[int(r.chip)], (r.y, r.x), self.radius),
+                return_pos=True,
+            )
             cnt, pos = out.values()
             counts.append(cnt)
             for yx in pos:
-                global_yx = np.array(yx) + np.array((r.y,r.x)) - self.radius/2
+                global_yx = (
+                    np.array(yx) + np.array((r.y, r.x)) - self.radius / 2
+                )
                 detections.append((int(r.chip), global_yx[0], global_yx[1]))
         self.df.loc[:, "counts"] = counts
-        self.viewer.add_points(data=centers, properties=self.df, text="counts", size=self.radius)
-        self.viewer.add_points(detections, size=20, face_color="#ffffff00", edge_color="#00ffff88")
+        self.viewer.add_points(
+            data=centers, properties=self.df, text="counts", size=self.radius
+        )
+        self.viewer.add_points(
+            detections, size=20, face_color="#ffffff00", edge_color="#00ffff88"
+        )
 
     def show_counts(self, counts):
-        self.counts  = counts
+        self.counts = counts
         print(counts)
 
     def _update_path(self):
@@ -89,9 +90,6 @@ class CountCells(QWidget):
         self.output_filename_widget.setText(self.out_path)
         self._combine(dry_run=True)
 
-    
-
     def reset_choices(self, event=None):
         self.select_centers.reset_choices(event)
         self.select_TRITC.reset_choices(event)
-
