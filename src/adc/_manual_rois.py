01dc05f5eaa29d3e32855eb2585fee73cf201845
Andrey Aristov <aaristov@pasteur.fr>
2022-11-24 14:36:30 +0100

    add cropping func

--- src/adc/_manual_rois.py
+++ src/adc/_manual_rois.py
@@ -1,41 +1,86 @@
-from magicgui import magicgui
+from functools import partial
+from typing import Union
+
+import dask.array as da
 import napari
-from napari.layers import Shapes
 import numpy as np
+from magicgui import magicgui
+from napari.layers import Image, Points, Shapes
+
 
 @magicgui
 def make_matrix(
-    Manual_ref_line:Shapes,
-    n_cols : int = 5,
-    n_rows : int = 5,
-    row_multiplier : float = 1.,
-    check : bool = True,
-    size : int = 300
-    
+    Manual_ref_line: Shapes,
+    n_cols: int = 5,
+    n_rows: int = 5,
+    row_multiplier: float = 1.0,
+    check: bool = True,
+    size: int = 300,
 ) -> napari.types.LayerDataTuple:
     manual_points = Manual_ref_line.data[0]
     assert len(manual_points == 2), "Select a line along your wells"
     manual_period = manual_points[1] - manual_points[0]
     col_period = manual_period / (n_cols - 1)
-    
+
     row_period = np.zeros_like(col_period)
     row_period[-2:] = np.array([col_period[-1], -col_period[-2]])
-    extrapolated_wells = np.stack([
-        manual_points[0] + \
-        col_period * i + \
-        row_period * j * row_multiplier + \
-        (col_period + row_period * row_multiplier) / 2 * k \
-        for k in range(2 * check) \
-        for i in range(n_cols) \
-        for j in range(n_rows)
-    ])
-    
-    return (
-        extrapolated_wells[:,-2:],
-        {"symbol": "square",
-         "size" : size,
-         "edge_color": "#ff0000",
-         "face_color": "#00000000"
-        },
-        "points"
+    extrapolated_wells = np.stack(
+        [
+            manual_points[0]
+            + col_period * i
+            + row_period * j * row_multiplier
+            + (col_period + row_period * row_multiplier) / 2 * k
+            for k in range(2 * check)
+            for i in range(n_cols)
+            for j in range(n_rows)
+        ]
     )
+
+    return (
+        extrapolated_wells[:, -2:],
+        {
+            "symbol": "square",
+            "size": size,
+            "edge_color": "#ff0000",
+            "face_color": "#00000000",
+        },
+        "points",
+    )
+
+
+@magicgui
+def crop_rois(
+    stack: Image,
+    ROIs: Points,
+) -> napari.types.LayerDataTuple:
+
+    data = stack.data
+    centers = ROIs.data
+    size = ROIs.size.max()
+
+    _crops = list(map(partial(crop_stack, stack=data, size=size), centers))
+    good_crops = filter(lambda a: a is not None, _crops)
+    meta = stack.metadata
+
+    return (da.stack(good_crops, axis=1), {"metadata": meta}, "image")
+
+
+def crop_stack(
+    center: np.ndarray, stack: np.ndarray, size: int
+) -> Union(np.ndarray, None):
+    """
+    Crops a square of the size `size` px from last two axes accrding to
+    2 last coordinates of the center.
+    Returns stack[...,size,size] if crop fits into the stack size, otherwise returns None.
+    """
+    assert stack.ndim >= 2
+    assert center.ndim == 1 & len(center) >= 2
+    s = size // 2
+    y, x = center[-2:].astype(int)
+    y1, y2 = y - s, y + s
+    x1, x2 = x - s, x + s
+    ylim, xlim = stack.shape[-2:]
+
+    if any([y1 < 0, x1 < 0, y2 > ylim, x2 > xlim]):
+        return
+    return stack[..., y1:y2, x1:x2]
