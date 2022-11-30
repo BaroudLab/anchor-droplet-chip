68a716bdbecda9384a4bfbb08ea549407c1d0b9b
Andrey Aristov <aaristov@pasteur.fr>
2022-11-30 14:28:04 +0100

    fix bug, add logging

--- src/adc/_manual_rois.py
+++ src/adc/_manual_rois.py
@@ -5,7 +5,10 @@ import napari
 import numpy as np
 from magicgui import magic_factory
 from napari.layers import Image, Points, Shapes
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 @magic_factory
 def make_matrix(
@@ -16,13 +19,20 @@ def make_matrix(
     check: bool = True,
     size: int = 300,
 ) -> napari.types.LayerDataTuple:
+    logger.debug(f"Ref line: {Manual_ref_line.data[0]}")
+    logger.debug(f"Scale: {Manual_ref_line.scale}")
     manual_points = Manual_ref_line.data[0] * Manual_ref_line.scale
+    logger.debug(f"manual_points: {manual_points}")
+
     assert len(manual_points == 2), "Select a line along your wells"
     manual_period = manual_points[1] - manual_points[0]
+    logger.debug(f"manual_period: {manual_period}")
     col_period = manual_period / (n_cols - 1)
+    logger.debug(f"col_period: {col_period}")
 
     row_period = np.zeros_like(col_period)
     row_period[-2:] = np.array([col_period[-1], -col_period[-2]])
+    logger.debug(f"row_period: {row_period}")
     extrapolated_wells = np.stack(
         [
             manual_points[0]
@@ -34,9 +44,10 @@ def make_matrix(
             for j in range(n_rows)
         ]
     )
+    logger.debug(f"extrapolated_wells: {extrapolated_wells}")
 
-    return (
-        extrapolated_wells[-2:],
+    out = (
+        extrapolated_wells[:, -2:],
         {
             "symbol": "square",
             "size": size,
@@ -45,6 +56,8 @@ def make_matrix(
         },
         "points",
     )
+    logger.debug(f"Returning: {out}")
+    return out
 
 
 @magic_factory
