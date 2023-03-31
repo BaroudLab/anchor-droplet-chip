cb0e996ef3d5d789adc30d07a79cd0594c59b2bd
Andrey Aristov <aaristov@pasteur.fr>
2023-03-31 18:47:29 +0200

    make zarr compatible with sub0stack etc, read crops, add logger

--- src/adc/_reader.py
+++ src/adc/_reader.py
@@ -1,9 +1,12 @@
 import json
+import logging
 import os
 
 import dask
 import nd2
 import pandas as pd
+
+logger = logging.getLogger(__name__)
 
 
 def napari_get_reader(path):
@@ -42,29 +45,31 @@ def read_zarr(path):
     try:
         attrs = json.load(open(os.path.join(path, ".zattrs")))
         info = attrs["multiscales"]["multiscales"][0]
+        dataset_paths = [
+            os.path.join(path, d["path"]) for d in info["datasets"]
+        ]
+        datasets = [dask.array.from_zarr(p) for p in dataset_paths]
     except Exception as e:
-        raise e
-
-    dataset_paths = [os.path.join(path, d["path"]) for d in info["datasets"]]
-    datasets = [dask.array.from_zarr(p) for p in dataset_paths]
+        logger.error(f"Error opening .zattr: {e}")
+        datasets = dask.array.from_zarr(path)
 
     try:
         channel_axis = info["channel_axis"]
         print(f"found channel axis {channel_axis}")
-    except KeyError:
-        channel_axis = None
-
     except Exception as e:
-        raise e
+        logger.debug(f"no info found {e}")
+        channel_axis = None
 
     try:
         contrast_limits = info["lut"]
-    except KeyError:
+    except Exception as e:
+        logger.debug("no info found")
         contrast_limits = None
 
     try:
         colormap = info["colormap"]
-    except KeyError:
+    except Exception as e:
+        logger.debug("no info found")
         colormap = None
 
     try:
@@ -75,6 +80,23 @@ def read_zarr(path):
     except Exception as e:
         print("name exception", e.args)
         name = os.path.basename(path)
+    meta = {"path": path}
+
+    try:
+        if "sizes" in info:
+            meta["sizes"] = info["sizes"]
+    except UnboundLocalError:
+        pass
+
+    try:
+        if not datasets[0].shape == tuple(meta["sizes"].values()):
+            logger.error(
+                f"dataset shape {datasets[0].shape} is not the same as size: {meta['sizes'].values()}"
+            )
+        else:
+            meta["dask_data"] = datasets[0]
+    except Exception as e:
+        logger.error(f"Error setting dask_data: {e}")
 
     output = [
         (
@@ -84,7 +106,7 @@ def read_zarr(path):
                 "colormap": colormap,
                 "contrast_limits": contrast_limits,
                 "name": name,
-                "metadata": {"path": path},
+                "metadata": meta,
             },
             "image",
         )
@@ -164,7 +186,9 @@ def read_nd2(path):
         # colormap = ["gray"]
     return [
         (
-ddata if max(ddata.shape) < 4000 else [ddata[...,::2**i,::2**i] for i in range(4)],
+            ddata
+            if max(ddata.shape) < 4000
+            else [ddata[..., :: 2**i, :: 2**i] for i in range(4)],
             {
                 "channel_axis": channel_axis,
                 "metadata": {
