2e2c6f1fbab71444f4946e0a88ad7b9ea869c29b
Andrey Aristov <aaristov@pasteur.fr>
2023-04-03 16:09:58 +0200

    test reader tif

--- src/adc/tests/test_reader.py
+++ src/adc/tests/test_reader.py
@@ -4,6 +4,7 @@ import shutil
 
 import dask.array as da
 import numpy as np
+import tifffile as tf
 from pytest import fixture
 from zarr_tools import convert
 
@@ -11,6 +12,28 @@ logger = logging.getLogger(__name__)
 logger.setLevel("DEBUG")
 
 ZPATH = "test.zarr"
+BTPATH = "btest.tif"
+STPATH = "stest.tif"
+
+
+@fixture
+def big_tif_dataset():
+    if os.path.exists(BTPATH):
+        os.remove(BTPATH)
+    arr = np.zeros((5, 2**13, 2**13), dtype="uint16")
+    tf.imwrite(BTPATH, arr, imagej=True)
+    logger.debug(f"{BTPATH} generated")
+    return BTPATH
+
+
+@fixture
+def sm_tif_dataset():
+    if os.path.exists(STPATH):
+        os.remove(STPATH)
+    arr = np.zeros((5, 2**5, 2**5), dtype="uint16")
+    tf.imwrite(STPATH, arr, imagej=True)
+    logger.debug(f"{STPATH} generated")
+    return STPATH
 
 
 @fixture
@@ -34,3 +57,25 @@ def test_read_zarr(zarr_dataset, make_napari_viewer):
         assert len(viewer.layers) == 5
     finally:
         shutil.rmtree(ZPATH)
+
+
+def test_read_small_tif(sm_tif_dataset, make_napari_viewer):
+    viewer = make_napari_viewer()
+    path = sm_tif_dataset
+    logger.debug(f"reading {path}")
+
+    viewer.open(path=path, plugin="anchor-droplet-chip")
+    assert len(viewer.layers) == 5
+    assert not viewer.layers[0].multiscale
+    assert isinstance(viewer.layers[0].data, da.Array)
+
+
+def test_read_big_tif(big_tif_dataset, make_napari_viewer):
+    viewer = make_napari_viewer()
+    path = big_tif_dataset
+    logger.debug(f"reading {path}")
+
+    viewer.open(path=path, plugin="anchor-droplet-chip")
+    assert len(viewer.layers) == 5
+    assert viewer.layers[0].multiscale
+    assert isinstance(viewer.layers[0].data[0], da.Array)
