2f4f00aad0f4c07497508211ac9f7766d3e0770e
Andrey Aristov <aaristov@pasteur.fr>
2023-06-06 17:51:54 +0200

    add numpy shape

--- src/adc/tools/log_decorator.py
+++ src/adc/tools/log_decorator.py
@@ -1,11 +1,20 @@
 import functools
 import logging
 
+import numpy as np
+
 
 def log(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        args_repr = [repr(a) for a in args]
+        args_repr = [
+            (
+                f"{repr(a)}"
+                if not isinstance(a, np.ndarray)
+                else f"numpy array of shape {a.shape} `{a[0]}`"
+            )
+            for a in args
+        ]
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
         signature = ", ".join(args_repr + kwargs_repr)
         logging.debug(
