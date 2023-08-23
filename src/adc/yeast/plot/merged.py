227cd2f264ed070620820ea054cc5d569a506635
Andrey Aristov <aaristov@pasteur.fr>
2023-08-23 11:00:45 +0200

    add final gfp state

--- src/adc/yeast/plot/merged.py
+++ src/adc/yeast/plot/merged.py
@@ -3,7 +3,136 @@ from typing import Iterable
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import re
 
+
+def add_gfp_final_state(merged_table: pd.DataFrame):
+    merged_table.loc[:, "ratio"] = merged_table.top10px / merged_table.mean_intensity
+
+    # merged_table = merged.merge_with_gfp_hour(map(pd.read_csv, tables))
+    merged_gfp = merged_table.query("channel == 'GFP'")
+    
+    gfp_positive_labels = merged_gfp.groupby(["path","label"]).max()[["GFP_positive"]]
+    gfp_positive_labels#.to_csv("pos0.csv")
+    
+    merged_table_with_final_gfp = merged_table.set_index(["path","label"]).join(gfp_positive_labels,  rsuffix="_final").reset_index()
+    return merged_table_with_final_gfp
+
+
+#plot below will show only the mCherry channel data
+#different colored lines indicate different labels
+#confidence interval shows spread of different paths for each label 
+
+def plot_top10px_for_channel(df, title="", channel_name="mCherry"):
+    '''
+    
+        Example usage:
+        _ = plot_top10px_for_channel(merged_table_with_final_gfp, channel_name="mCherry")
+
+    '''
+    
+    # Filter the DataFrame for the specific channel
+    df_channel = df[df['channel'] == channel_name]
+    
+    # Create a 2x1 subplot layout
+    fig, ax = plt.subplots(ncols=2, figsize=(10, 6))
+    
+    df_channel.label = df_channel.label.astype("category")
+    # Create a line plot for top10px over GFPhour with specific settings
+    sns.lineplot(
+        ax=ax[0],
+        data=df_channel,
+        x='GFPhour',  # Use GFPhour for x-axis
+        y='top10px',
+        # hue='label',
+        palette='Set2',
+        style_order=[True, False],
+        style="GFP_positive_final",
+        # style="path",
+        legend=False  # Remove the legend
+    )
+    ax[0].set_title(title)
+    
+    # Create a line plot for ratio over GFPhour with specific settings
+    sns.lineplot(
+        ax=ax[1],
+        data=df_channel,
+        x='GFPhour',  # Use GFPhour for x-axis
+        y='ratio',
+        # hue='label',
+        style_order=[True, False],
+        style="GFP_positive_final",
+        legend=False,  # Remove the legend
+        palette='Set2'
+    )
+    ax[1].legend(loc=(1.1, 0))  # Adjust the legend position
+    ax[1].set_title(title)
+    
+    plt.tight_layout()  # Adjust layout for better spacing
+    # return fig
+
+
+def plot_top10px_for_channel_gfp_final(df, title="", channel_name="mCherry", ylim1=(100, 400), ylim2=(1, 2), xlim=(-10, 6)):
+    # Calculate the ratio column by dividing top10px by mean_intensity
+    df.loc[:, "ratio"] = df.top10px.values / df.mean_intensity.values
+    
+    # Filter the DataFrame for the specific channel
+    df_channel = df
+    
+    # Create a 2x1 subplot layout
+    fig, ax = plt.subplots(ncols=2, sharex=True, figsize=(10, 6))
+    
+    df_channel.label = df_channel.label.astype("category")
+    # Create a line plot for top10px over GFPhour with specific settings
+    sns.lineplot(
+        size="channel",
+        ax=ax[0],
+        data=df_channel,
+        x='GFPhour',  # Use GFPhour for x-axis
+        y='top10px',
+        hue='label',
+        palette='Set2',
+        style="GFP_positive_final",
+        style_order=[True, False],
+        legend=False  # Remove the legend
+    )
+    ax[0].set_title(title)
+    ax[0].set_ylim(*ylim1)
+    
+    # Create a line plot for ratio over GFPhour with specific settings
+    sns.lineplot(
+        size="channel",
+        ax=ax[1],
+        data=df_channel,
+        x='GFPhour',  # Use GFPhour for x-axis
+        y='ratio',
+        hue='label',        
+        style_order=[True, False],
+        style="GFP_positive_final",
+        # legend=False,  # Remove the legend
+        palette='Set2'
+    )
+    ax[1].legend(loc=(1.1, 0))  # Adjust the legend position
+    ax[1].set_title(title)
+    ax[1].set_ylim(*ylim2)
+    ax[1].set_xlim(*xlim)
+    
+    plt.tight_layout()  # Adjust layout for better spacing
+    # return fig
+
+def plot_intensitites_with_gfp_final(merged_table_with_final_gfp, ylim1=(100, 400), ylim2=(1, 2), xlim=(-10, 6)):
+    for p in merged_table_with_final_gfp.path.unique():
+        print(f"path=='{p}'")
+        try:
+            plot_top10px_for_channel_gfp_final(
+                merged_table_with_final_gfp.query(f"path=='{p}'"), 
+                channel_name="mCherry", 
+                title = re.search(r"pos\d+", p).group(),
+                ylim1=ylim1,
+                ylim2=ylim2,
+                xlim=xlim)
+        except Exception as e:
+            print(e, e.args)
 
 def get_gfp_positive_number(df):
     return (
