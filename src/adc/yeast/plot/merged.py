94e944cda5ebec672e7dbb16e3a1fc35b92bf9ec
Andrey Aristov <aaristov@pasteur.fr>
2023-09-04 14:58:10 +0200

    savefig

--- src/adc/yeast/plot/merged.py
+++ src/adc/yeast/plot/merged.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+from .single import get_title
+
 
 def get_gfp_positive_number(df):
     return (
@@ -66,11 +68,13 @@ def plot_10(df, save_path="all-top10px.png"):
     plt.rc("font", family="Arial")
     data = df.query("mask=='cellpose' and channel=='mCherry' ")
     fig, ax1 = plt.subplots(figsize=(5, 5), dpi=150, facecolor="w")
+    data.loc[:, "title"] = data["path"].map(get_title)
     sns.lineplot(
         ax=ax1,
         x=data.hours,
         y=data.top10px / data.mean_intensity,
-        hue=data.path,
+        hue=data.title,
+        legend=False,
     )
     sns.lineplot(
         ax=ax1,
@@ -79,23 +83,16 @@ def plot_10(df, save_path="all-top10px.png"):
         # hue=data.path,
         linewidth=5,
         color="k",
+        legend=False,
     )
-    # ax2=ax1.twinx()
-    # get_gfp_positive_number(df).plot(ax=ax2, label="GFP positive cells", linewidth=3)
-    # get_cell_number(df).plot(ax=ax2, label="total number of cells", linewidth=2)
-    # sns.lineplot(ax=ax2,
-    #              x="frame",
-    #              y="GFP_positive",
-    #              label="GFP positive cells",
-    #              data=df
-    #             )
     ax1.set_ylim(1, 2.5)
     ax1.set_ylabel("norm intensity")
     # ax2.set_ylabel("cell count")
     # ax2.legend(loc="upper right")
     ax1.legend(loc="upper right")
     ax1.set_title("top 10 px")
-    fig.savefig(save_path)
+    if save_path:
+        fig.savefig(save_path)
     # plt.close()
 
 
@@ -108,6 +105,7 @@ def plot_10_gfp(df, save_path="all-top10px-gfphour.png"):
         x=data.GFPhour,
         y=data.top10px / data.mean_intensity,
         hue=data.path,
+        legend=False,
     )
     sns.lineplot(
         ax=ax1,
@@ -116,6 +114,7 @@ def plot_10_gfp(df, save_path="all-top10px-gfphour.png"):
         # hue=data.path,
         linewidth=5,
         color="k",
+        legend=False,
     )
     ax2 = ax1.twinx()
     # get_gfp_positive_number(df).plot(ax=ax2, label="GFP positive cells", linewidth=3)
@@ -130,7 +129,7 @@ def plot_10_gfp(df, save_path="all-top10px-gfphour.png"):
     ax1.set_ylabel("norm intensity")
     # ax2.set_ylabel("cell count")
     # ax2.legend(loc="upper right")
-    ax1.legend(loc="upper right")
+    # ax1.legend(loc="upper right")
     ax1.set_title("top 10 px")
     fig.savefig(save_path)
     # plt.close()
